fcce95d70e70bc56c97abe5799f040570b579c34
Ana Paula <anapaulaghis@gmail.com>
2025-05-03 14:44:24 -0300

    `Updated various assets and scripts for the Pixel Adventure game, including bullet prefab, tilemap, enemy animations, and health system.`

--- Assets/Scripts/HealthBossScript.cs
+++ Assets/Scripts/HealthBossScript.cs
@@ -1,30 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBossScript : MonoBehaviour
 {
     public int baseHealth = 100;
-    public int strawberriesThreshold = 20; 
-    public int healthReduction = 10; 
+    public int strawberriesThreshold = 20;
+    public int healthReduction = 1;
     private int bossHealth;
+    private int maxHealth;
+    public Slider healthBarSlider;
 
     void Start()
     {
         int totalStrawberries = GameController.totalStrawberriesCollected;
-
-        // Calcula quantas vezes o jogador passou do limite de 20 morangos
         int reduction = (totalStrawberries / strawberriesThreshold) * healthReduction;
 
         bossHealth = Mathf.Max(baseHealth - reduction, 0);
+        maxHealth = bossHealth;
 
-        Debug.Log("Morangos coletados: " + totalStrawberries);
-        Debug.Log("Vida do Boss: " + bossHealth);
+        UpdateHealthBar();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int amount)
     {
-        
+        bossHealth -= amount;
+        bossHealth = Mathf.Max(bossHealth, 0);
+        UpdateHealthBar();
+
+        Debug.Log("Boss recebeu dano! Vida restante: " + bossHealth);
+
+        if (bossHealth <= 0)
+        {
+            Debug.Log("Boss derrotado!");
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        if (healthBarSlider != null)
+        {
+            healthBarSlider.value = bossHealth;
+        }
     }
 }
